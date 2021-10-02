3e653e0a850238a2b286b0ecefbebdcf79f091f7
paul <paul.giraudet@epitech.digital>
2021-10-02 12:02:54 +0200

    landing_page

---
+++ landing_page/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+    "use strict";
+
+$(document).ready(function(){
+	
+$(".app-features").owlCarousel({
+
+    nav:false,
+    items:1,
+    dots:true,
+    margin:0,
+
+});
+	
+});
+
+})(jQuery);	
+
+
